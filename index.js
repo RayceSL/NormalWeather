36a7aeed0ac402f36b67ed95fb31455f13c51108
RayceSL <raycel@spitwspots.com>
2024-07-16 12:00:58 -0800

    asks for location and time with formatting

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import chalk from "chalk";
 import number from "@inquirer/number";
+import select from "@inquirer/select";
 import { createSpinner } from "nanospinner";
 import { ValueNoise } from "value-noise-js";
 
 var lat;
 var long;
+var hour;
+var min;
+var meridiem;
+var timeFormat;
+
 var seed;
 var seed = lat + long;
 
@@ -15,15 +21,68 @@ async function welcome() {
 async function askLoc() {
     lat = await number({ message: "Enter your latitude" });
     long = await number({message: "Enter your longitude"});
+    timeFormat = await select({
+        message: "12 hour or 24 hour time?",
+        choices: [
+            {
+                name: "12 hr",
+                value: 0
+            },
+            {
+                name: "24 hr",
+                value: 1
+            }
+        ]
+    });
+
+    switch (timeFormat) {
+        case 0:
+            hour = await number({message: "Enter the hour"});
+            min = await number({message: "Enter the minute"});
+            meridiem = await select({
+                message: "AM or PM?",
+                choices: [
+                    {
+                        name: "AM",
+                        value: 0
+                    },
+                    {
+                        name: "PM",
+                        value: 12
+                    }
+                ]
+            });
+            break;
+        case 1:
+            hour = await number({message: "Enter the hour"});
+            min = await number({message: "Enter the minute"});
+            meridiem = 0;
+            break;
+    }
 }
 
 async function perlin() {
-    const noise = new ValueNoise(seed)
+    var noise = new ValueNoise(seed)
+    var time = hour + meridiem + min * 0.01
+    var value = noise.evalX(time);
 
-    var x = 1;
+    switch (timeFormat) {
+        case 0:
 
-    // Evaluate at x
-    var value = noise.evalX(x);
+            switch (meridiem) {
+                case 0:
+                    console.log(`Weather at ${hour}:${min} AM at ( ${lat}, ${long} ):`);
+                    break;
+                case 12:
+                    console.log(`Weather at ${hour}:${min} PM at ( ${lat}, ${long} ):`);
+                    break;
+            }
+            break;
+
+        case 1:
+            console.log(`Weather at ${hour + meridiem}:${min} at ( ${lat}, ${long} ):`);
+            break;
+    }
     console.log(value);
 }
 
