5d1189c2e57900be0681b9350bfb42ed60fc22f2
RayceSL <raycel@spitwspots.com>
2024-07-16 11:25:24 -0800

    location is inputted

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 import chalk from "chalk";
-import inquirer from "inquirer";
+import number from "@inquirer/number";
 import { createSpinner } from "nanospinner";
+import { ValueNoise } from "value-noise-js";
 
-console.log(chalk.bgBlueBright.whiteBright("Hello, ") + chalk.bgBlueBright.yellowBright("sunshine!"));
+var lat;
+var long;
+var seed;
+var seed = lat + long;
+
+async function welcome() {
+    console.log(chalk.bgBlueBright.whiteBright("Hello, ") + chalk.bgBlueBright.yellowBright("sunshine!"));
+}
+
+async function askLoc() {
+    lat = await number({ message: "Enter your latitude" });
+    long = await number({message: "Enter your longitude"});
+}
+
+async function perlin() {
+    const noise = new ValueNoise(seed)
+
+    var x = 1;
+
+    // Evaluate at x
+    var value = noise.evalX(x);
+    console.log(value);
+}
+
+await welcome();
+await askLoc();
+await perlin();
