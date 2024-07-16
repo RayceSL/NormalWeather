b7d3a60084031be7892beae6151de154a948b2a6
RayceSL <raycel@spitwspots.com>
2024-07-16 14:43:21 -0800

    'Live' updating weather

--- index.js
+++ index.js
@@ -10,14 +10,13 @@ var hour;
 var min;
 var meridiem;
 var timeFormat;
-
 var seed;
-var seed = lat + long;
 
 async function welcome() {
     console.log(chalk.bgBlueBright.whiteBright("Hello, ") + chalk.bgBlueBright.yellowBright("sunshine!"));
 }
 
+// Sets the seed
 async function askLoc() {
     lat = await number({ message: "Enter your latitude" });
     long = await number({message: "Enter your longitude"});
@@ -59,12 +58,15 @@ async function askLoc() {
             meridiem = 0;
             break;
     }
+
+    seed = lat + long;
 }
 
+// Sets the weather using seed
 async function perlin() {
-    var noise = new ValueNoise(seed)
-    var time = hour + meridiem + min * 0.01
-    var value = noise.evalX(time);
+    var noise = new ValueNoise(seed);
+    var time = hour + meridiem + (min * 0.01);
+    var weather = Math.floor(noise.evalX(time) * 100);
 
     switch (timeFormat) {
         case 0:
@@ -80,10 +82,70 @@ async function perlin() {
             break;
 
         case 1:
-            console.log(`Weather at ${hour + meridiem}:${min} at ( ${lat}, ${long} ):`);
+            console.log(`Weather at ${hour + meridiem}:${min} at ( ${lat} , ${long} ):`);
             break;
     }
-    console.log(value);
+
+    timeScroll();
+
+    async function timeScroll() {
+        let i = 0;
+        while (i <= 60) {
+            var noise = new ValueNoise(seed);
+            var time = hour + meridiem + (min * 0.01);
+            var weather = Math.floor(noise.evalX(time) * 100);
+            getWeather();
+
+            function getWeather() {
+                if (weather == 100) {
+                    console.log("Downpour of scalding hot coffee");
+
+                } else if (weather >= 90 && weather <= 99) {
+                    console.log("Hail of peanuts");
+                    
+                } else if (weather >= 80 && weather <= 99) {
+                    console.log("Angel death");
+
+                } else if (weather >= 70 && weather <= 89) {
+                    console.log("Sonder");
+        
+                } else if (weather >= 60 && weather <= 79) {
+                    console.log("Blood rain");
+
+                } else if (weather >= 50 && weather <= 69) {
+                    console.log("Melancholic thunder");
+        
+                } else if (weather >= 40 && weather <= 59) {
+                    console.log("Cacophony of screams");
+
+                } else if (weather >= 30 && weather <= 39) {
+                    console.log("Your favorite YouTuber is cancelled");
+        
+                } else if (weather >= 20 && weather <= 39) {
+                    console.log("Blinding sunlight");
+
+                } else if (weather >= 10 && weather <= 19) {
+                    console.log("Devilfrost");
+        
+                } else if (weather <= 9) {
+                    console.log("Bone wind");
+        
+                } else {
+                    console.log("Blackholey");
+                }
+            }
+
+            min = min + 10;
+            i++;
+            await sleep(5000);
+        }
+    }
+
+    function sleep(ms) {
+        return new Promise((resolve) => {
+            setTimeout(resolve, ms);
+        });
+    }
 }
 
 await welcome();
